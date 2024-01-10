e5049824b39b6a3e58c5a7436b57ca4aac6668d0
HeagekWHAT <heagekwhat@yandex.ru>
2024-01-10 19:51:06 +0300

    Completed homework

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LabelCounter: UILabel!
+    
+    var LabelCounterString = "Значение счётчика:"
+    
+    var LabelCounterInt = 0
+    
+    @IBOutlet weak var ButtonCounter: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction private func PressButtonCouner(_ sender: Any) {
+        LabelCounterInt += 1
+        if LabelCounterInt >= 0 {
+            LabelCounter.text = "\(LabelCounterString) \(String(LabelCounterInt))"
+        }
+    }
 }
-
